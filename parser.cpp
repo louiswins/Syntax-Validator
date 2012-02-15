678d95768097dd5445bf905419467c0ae8e4ccb3
Louis Wilson <louis.wilson@asu.edu>
2012-02-15 13:08:54 -0700

    First version of parser

--- parser.cpp
+++ parser.cpp
@@ -1,82 +1,257 @@
 #include "parser.h"
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
 using namespace std;
 
-ptree::ptr parser::read_expression() {
-	ptree::ptr ret = ampersand_op();
-	if (!match("\n")) {
-		_l.flush_line();
-		return ptree::ptr();
+namespace {
+	void syntax_error(unsigned line_no) {
+		cout << "line " << line_no << " syntax error\n";
+		exit(EXIT_FAILURE);
 	}
-	return ret;
+	void identifier_redef_error(unsigned line_no) {
+		cout << "line " << line_no << " static semantic error - "
+			"identifier redefined\n";
+	}
+	void identifier_undef_error(unsigned line_no) {
+		cout << "line " << line_no << " static semantic error - "
+			"identifier undefined\n";
+		exit(EXIT_FAILURE);
+	}
+	void const_assign_error(unsigned line_no) {
+		cout << "line " << line_no << "static semantic error - "
+			"invalid assignment\n";
+	}
 }
 
-ptree::ptr parser::ampersand_op() {
-	ptree::ptr ret = percent_op();
-
-	while (ret && _l.token() == "&") {
-		match("&");
-		ptree::ptr tmp = percent_op();
-		if (!tmp) return ptree::ptr();
-		ret = ptree::ptr(new amp_operator(ret, tmp));
+void parser::verify_program() {
+	block();
+	if (_l.hasToken()) {
+		syntax_error(_l.line_no());
 	}
-
-	return ret;
 }
 
-ptree::ptr parser::percent_op() {
+void parser::block() {
+	_s.push_block();
+	while (is_declaration(_l.token())) {
+		declaration();
+	}
+	while (is_statement(_l.token())) {
+		statement();
+	}
+	_s.pop_block();
+}
+
+void parser::declaration() {
+	if (_l.token() == "var") {
+		variable_declaration();
+	} else if (_l.token() == "const") {
+		constant_declaration();
+	} else {
+		syntax_error(_l.line_no());
+	}
+}
+void parser::variable_declaration() {
+	match("var");
+	if (!id_definition(_l.token())) {
+		identifier_redef_error(_l.line_no());
+	} else {
+		_s.decl_var(_l.token());
+	}
+	match();
+	while (_l.token() == ",") {
+		match(",");
+		if (!id_definition(_l.token())) {
+			identifier_redef_error(_l.line_no());
+		} else {
+			_s.decl_var(_l.token());
+		}
+		match();
+	}
+}
+void parser::constant_declaration() {
+	match("const");
+	if (!id_definition(_l.token())) {
+		identifier_redef_error(_l.line_no());
+	} else {
+		_s.decl_const(_l.token());
+	}
+	match();
+	match("=");
+	number();
+}
+
+void parser::statement() {
+	if (_l.token() == "print") {
+		print_stmt();
+	} else if (_l.token() == "if") {
+		if_stmt();
+	} else if (_l.token() == "do") {
+		do_stmt();
+	} else {
+		assignment();
+	}
+}
+
+void parser::assignment() {
+	symtab::var_type type = id_lookup(_l.token());
+	if (type == symtab::undef) {
+		identifier_undef_error(_l.line_no());
+	} else if (type == symtab::constant) {
+		const_assign_error(_l.line_no());
+	}
+	match();
+	match(":=");
+	expression();
+}
+
+void parser::print_stmt() {
+	match("print");
+	expression();
+}
+void parser::if_stmt() {
+	match("if");
+	while (_l.token() == "do") {
+		do_block();
+	}
+	match("end");
+}
+void parser::do_stmt() {
+	match("loop");
+	while (_l.token() == "do") {
+		do_block();
+	}
+	match("end");
+}
+void parser::do_block() {
+	match("do");
+	expression();
+	match("->");
+	block();
+	match("end");
+}
+
+void parser::expression() {
+	simple();
+	if (is_relop(_l.token())) {
+		match();
+		simple();
+	}
+}
+void parser::simple() {
+	uni_term();
+	while (_l.token() == "&") {
+		match();
+		uni_term();
+	}
+}
+void parser::uni_term() {
 	if (_l.token() == "%") {
-		match("%");
-		ptree::ptr tmp = percent_op();
-		if (!tmp) return ptree::ptr();
-		return ptree::ptr(new pct_operator(tmp));
+		match();
+		uni_term();
 	} else {
-		return at_op();
+		term();
 	}
 }
-
-ptree::ptr parser::at_op() {
-	ptree::ptr ret = term();
-
-	if (ret && _l.token() == "@") {
-		match("@");
-		ptree::ptr tmp = at_op();
-		if (!tmp) return ptree::ptr();
-		ret = ptree::ptr(new at_operator(ret, tmp));
+void parser::term() {
+	factor();
+	if (_l.token() == "@") {
+		match();
+		term();
 	}
-	return ret;
 }
-
-ptree::ptr parser::term() {
+void parser::factor() {
 	if (_l.token() == "(") {
-		match("(");
-		ptree::ptr ret = ampersand_op();
-		if (!match(")")) return ptree::ptr();
-		return ret;
-	} else {
-		return number();
+		match();
+		expression();
+		match(")");
+	} else if (is_number(_l.token())) {
+		number();
+	} else if (is_ident(_l.token())) {
+		if (id_lookup(_l.token()) == symtab::undef) {
+			identifier_undef_error(_l.line_no());
+		} else {
+			match();
+		}
 	}
 }
 
-ptree::ptr parser::number() {
+void parser::number() {
+	if (!is_number(_l.token())) {
+		syntax_error(_l.line_no());
+	} else {
+		// sillily, we don't even do anything with it yet.
+		match();
+	}
+}
+
+
+
+bool parser::id_definition(const string& tok) {
+	if (!is_ident(tok)) {
+		syntax_error(_l.line_no());
+	}
+	return _s.exists_in_block(tok);
+}
+symtab::var_type parser::id_lookup(const string& tok) {
+	if (!is_ident(tok)) {
+		syntax_error(_l.line_no());
+	}
+	return _s.find(tok);
+}
+
+inline bool parser::is_reserved_word(const string& tok) {
+	return (tok == "const" ||
+		tok == "do"    ||
+		tok == "end"   ||
+		tok == "loop"  ||
+		tok == "var");
+}
+inline bool parser::is_declaration(const string& tok) {
+	return (tok == "var" || tok == "const");
+}
+inline bool parser::is_statement(const string& tok) {
+	return (tok == "print" ||
+		tok == "if"    ||
+		tok == "do"    ||
+		is_ident(tok));
+}
+inline bool parser::is_relop(const string& tok) {
+	return (tok == "="  ||
+		tok == "<"  ||
+		tok == ">"  ||
+		tok == "/=" ||
+		tok == "<=" ||
+		tok == ">=");
+}
+inline bool parser::is_ident(const string& tok) {
+	if (is_reserved_word(tok)) return false;
+	for (string::const_iterator i = tok.begin(); i != tok.end(); ++i) {
+		if (!isalpha(*i)) return false;
+	}
+	return true;
+}
+
+bool parser::is_number(const string& tok) {
 	int ret;
 	if (!(istringstream(_l.token()) >> ret)) {
-		// the conversion failed; it's not actually a number.
-		return ptree::ptr();
+		return false;
 	}
-	match(_l.token());
-	return ptree::ptr(new int_literal(ret));
+	return true;
 }
 
-bool parser::match(const string tok) {
+bool parser::match(const char *tok) {
 	// lexical error
 	if (!_l.good()) return false;
 	if (tok == _l.token()) {
 		_l.getToken();
 		return true;
 	} else {
-		cout << "line " << _l.line_no() << " syntax error\n";
-		return false;
+		syntax_error(_l.line_no());
 	}
+}
+bool parser::match() {
+	if (!_l.good()) return false;
+	_l.getToken();
+	return true;
 }
--- parser.h
+++ parser.h
@@ -2,24 +2,48 @@
 #define PARSER_H
 
 #include "lexer.h"
+#include "symtab.h"
 #include <string>
 
 class parser {
 public:
 	parser() {}
-	parser(lexer l) : _l(l) {}
+	parser(const lexer& l) : _l(l) {}
 
-	ptree::ptr read_expression();
-	bool has_expression() const { return _l.hasToken(); }
+	void verify_program();
+
 private:
-	ptree::ptr ampersand_op();
-	ptree::ptr percent_op();
-	ptree::ptr at_op();
-	ptree::ptr term();
-	ptree::ptr number();
-	bool match(const std::string tok);
+	void block();
+	void declaration();
+	void variable_declaration();
+	void constant_declaration();
+	void statement();
+	void assignment();
+	void print_stmt();
+	void if_stmt();
+	void do_stmt();
+	void do_block();
+	void expression();
+	void simple();
+	void uni_term();
+	void term();
+	void factor();
+	void number();
+
+	bool id_definition(const std::string& tok);
+	symtab::var_type id_lookup(const std::string& tok);
+
+	bool is_reserved_word(const std::string& tok);
+	bool is_declaration(const std::string& tok);
+	bool is_statement(const std::string& tok);
+	bool is_relop(const std::string& tok);
+	bool is_ident(const std::string& tok);
+	bool is_number(const std::string& tok);
+	bool match(const char *tok);
+	bool match();
 
 	lexer _l;
+	symtab _s;
 };
 
 #endif /* PARSER_H */
