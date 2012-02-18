009e6ee7ebca522c680c7b3288555b2bc0008b20
Louis Wilson <louis.wilson@asu.edu>
2012-02-18 00:01:55 -0700

    Take advantage of the STL and boost

--- symtab.cpp
+++ symtab.cpp
@@ -1,16 +1,25 @@
-#include "symtab.h"
 #include <string>
+#include <algorithm>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+#include "symtab.h"
+using boost::lambda::bind;
+using boost::lambda::_1;
 
-const symbol symtab::find(const std::string& which) const {
-	for (table::const_reverse_iterator i = tab.rbegin(); i != tab.rend(); ++i) {
-		if (i->name() == which) {
-			return *i;
-		}
-	}
-	return symbol("", symbol::undef);
+// Here we search through all possibilities, so we can use the stl find_if.
+symbol symtab::find(const std::string& which) const {
+	table::const_reverse_iterator result = std::find_if(tab.rbegin(), tab.rend(),
+			bind(&symbol::name, _1) == which);
+	return (result == tab.rend()) ?
+		symbol("", symbol::undef) :
+		*result;
 }
+// Here we bail out partway through, so it is unreasonable to implement in
+// terms of stl (at least with the std::deque representation we have)
 bool symtab::exists_in_block(const std::string& which) const {
-	for (table::const_reverse_iterator i = tab.rbegin(); i != tab.rend(); ++i) {
+	table::const_reverse_iterator i = tab.rbegin(),
+		end = tab.rend();
+	for (; i != end; ++i) {
 		if (*i == block) {
 			return false;
 		}
